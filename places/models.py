19e99afc0bcb04613cc94a84c2fae6eec39dab12
0viktory0 <36894469+0viktory0@users.noreply.github.com>
2023-02-25 17:50:55 +0300

    extend place model in admin

--- places/models.py
+++ places/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Place(models.Model):
     title = models.CharField('Название', max_length=200, db_index=True)
     description_short = models.TextField('Короткое описание', blank=True)
     description_long = models.TextField('Длинное описание', blank=True)
     lng = models.FloatField('Широта')
     lat = models.FloatField('Долгота')
+
 
     def __str__(self):
         return f'{self.title}'
