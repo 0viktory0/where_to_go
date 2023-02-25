a325cb7c68914c015d57dbbb07beae252093d762
0viktory0 <36894469+0viktory0@users.noreply.github.com>
2023-02-25 19:05:49 +0300

    added sorted fields

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from adminsortable2.admin import SortableAdminBase, SortableAdminMixin, SortableInlineAdminMixin
 from .models import Place, Image
 
 
-class ImageInline(admin.TabularInline):
+class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
     fields = ['place', 'image', 'get_preview']
     readonly_fields = ['get_preview']
@@ -14,9 +15,10 @@ class ImageInline(admin.TabularInline):
         )
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
+    list_display = ['title',]
     inlines = [ImageInline]
 
 @admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
+class ImageAdmin(SortableAdminMixin, admin.ModelAdmin):
     pass
--- places/models.py
+++ places/models.py
@@ -12,13 +12,22 @@ class Place(models.Model):
     def __str__(self):
         return f'{self.title}'
 
+    class Meta(object):
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
+
 
 class Image(models.Model):
     image = models.ImageField('Картинка')
-    number = models.PositiveIntegerField('Позиция', default=0, blank=True)
+    number = models.PositiveIntegerField('Позиция', default=0, blank=True, null=False,)
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
 
 
 
     def __str__(self):
         return f'{self.number}. {self.place.title}'
+
+    class Meta(object):
+        ordering = ['number']
+        verbose_name = 'Картинка'
+        verbose_name_plural = 'Картинки'
