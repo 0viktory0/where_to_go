2c49869438dd1865ddc712bfc7988a93434c01aa
0viktory0 <36894469+0viktory0@users.noreply.github.com>
2023-02-25 14:54:16 +0300

    added geojson data

--- mysite/views.py
+++ mysite/views.py
@@ -1,36 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from places.models import Place, Image
 
 def show_main(request):
-    places = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.62, 55.793676]
-                },
-                "properties": {
-                    "title": "«Легенды Москвы",
-                    "placeId": "moscow_legends",
-                    "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
+    places = Place.objects.all()
+    places_description = []
+    for place in places:
+        description = {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [place.lng, place.lat]
+                    },
+                    "properties": {
+                        "title": place.title,
+                        "placeId": place.id,
+                        "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
+                    }
                 }
-            },
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.64, 55.753676]
-                },
-                "properties": {
-                    "title": "Крыши24.рф",
-                    "placeId": "roofs24",
-                    "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
-                }
-            }
-        ]
-    }
+            ]
+        }
+        places_description.append(description)
 
-    return render(request, 'index.html', {'places_geojson': places})
+    return render(request, 'index.html', {'places_geojson': places_description})
